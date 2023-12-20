32fceeeffba914a20572181ba516a35b9920bb49
Francisco Timez <franciscotimez@gmail.com>
2023-12-20 17:22:42 -0300

    save

--- 02-todo/src/app.module.ts
+++ 02-todo/src/app.module.ts
@@ -1,7 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault
+      ]
+    }),
+    HelloWorldModule],
   controllers: [],
   providers: [],
 })
