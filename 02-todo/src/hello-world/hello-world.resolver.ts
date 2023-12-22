b3dbbdd874995336e76bcb3e8f4c0e46950fb9fd
Francisco Timez <franciscotimez@gmail.com>
2023-12-22 09:39:00 -0300

    fin seccion 3

--- 02-todo/src/hello-world/hello-world.resolver.ts
+++ 02-todo/src/hello-world/hello-world.resolver.ts
@@ -1,11 +1,33 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-  @Query(() => String)
+  @Query(() => String, {
+    name: "hello",
+    description: "Retorna Hola mundo"
+  })
   helloWorld(): string {
     return "Hola mundo";
   }
 
+  @Query(() => Float, {
+    name: "randomNumber",
+    description: "Retorna un numero aleatorio"
+  })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: "randomFromZeroTo",
+    description: "Retorna un numero entero aleatorio desde cero."
+  })
+  getRandomFromZeroTo(@Args('to', {
+    type: () => Int,
+    defaultValue: 10,
+    nullable: true
+  }) to: number): number {
+    return Math.floor(Math.random() * to);
+  }
 }
