141f383b4bf877d4c3d72d9838ee96a4119dfdd6
Francisco Timez <franciscotimez@gmail.com>
2023-12-26 16:12:42 -0300

    updateItem

--- 03-anylist/src/items/items.service.ts
+++ 03-anylist/src/items/items.service.ts
@@ -30,8 +30,10 @@ export class ItemsService {
     return item;
   }
 
-  update(id: number, updateItemInput: UpdateItemInput) {
-    return `This action updates a #${id} item`;
+  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+    const item = await this.itemsRepository.preload(updateItemInput);
+    if (!item) throw new NotFoundException(`Item ${id} not found.`);
+    return await this.itemsRepository.save(item);
   }
 
   remove(id: number) {
