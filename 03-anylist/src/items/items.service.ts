2c48a0d541d9872d8dc72e9a9974f89989b974f0
Francisco Timez <franciscotimez@gmail.com>
2023-12-26 15:42:49 -0300

    findOne

--- 03-anylist/src/items/items.service.ts
+++ 03-anylist/src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
 import { Repository } from 'typeorm';
@@ -17,12 +17,17 @@ export class ItemsService {
     return await this.itemsRepository.save(newItem);
   }
 
-  findAll() {
-    return `This action returns all items`;
+  async findAll(): Promise<Item[]> {
+    // todo: filtrar
+    // todo: paginar
+    // todo: por user
+    return this.itemsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({ id });
+    if (!item) throw new NotFoundException(`Item ${id} not found.`);
+    return item;
   }
 
   update(id: number, updateItemInput: UpdateItemInput) {
